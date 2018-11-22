bd45b2324cda450bd0da15c848a89416eac10bdb
Israel Machovec <imachovec@bignerdranch.com>
2018-11-22 11:04:14 -0500

    update container modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <h2>I'm a Product Manager and Creator</h2>
     <p>
       What do I like to do? Lots of course but definitely enjoy building
       websites.
