6d3385df7efa45b478b7bafb8574776a713d821a
Israel Machovec <imachovec@bignerdranch.com>
2018-11-25 20:47:09 -0500

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,29 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default () => (
+export default ({data}) => (
   <Layout>
-    <h2>I'm a Product Manager and Creator</h2>
+    <h2>I'm a</h2>
     <p>
-      What do I like to do? Lots of course but definitely enjoy building
-      websites.
+      We're the only site running on your computer dedicated to showing the best
+      photos and videos of pandas eating lots of food.
     </p>
+    <div>
+   <img
+     src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+     alt="Group of pandas eating bamboo"
+   />
+ </div>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
---
+++ src/posts/resume.md
@@ -0,0 +1,9 @@
+---
+path: "/resume"
+date: "2018-11-11"
+title: "Israel Machovec"
+---
+
+### Israel Machovec  
+Atlanta, GA  
+[Connect on LinkedIn](https://www.linkedin.com/in/israelmachovec/)  
